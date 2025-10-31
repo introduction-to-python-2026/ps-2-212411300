367fb93135b5bc7fd315b7b8fbe8b6cdab40498f
212411300 <racheliseitz1@gmail.com>
2025-10-31 21:46:42 +0200

    שיעורי בית 2 גרסא 9

--- math_utils.py
+++ math_utils.py
@@ -1,7 +1,7 @@
 def find_max_number(num1, num2, num3):
-  if num1>num2 and num1>num3:
+  if num1>=num2 and num1>=num3:
       return(num1)
-  elif num2>num1 and num2>num3:
+  elif num2>=num1 and num2>=num3:
     return(num2)
   else:
     return(num3)
@@ -13,4 +13,4 @@ def find_mean(num1, num2, num3):
 def find_mean_std(num1, num2, num3):
     mean = ((num1+num2+num3)/3)
     mean_std = ((num1-mean)**2+(num2-mean)**2+(num3-mean)**2/3)**0.5
-    return(mean,mean_std)
+    return(mean,mean_std) 
